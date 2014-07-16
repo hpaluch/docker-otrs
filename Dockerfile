e22ff303d5d817aeaa748d1c0fdc358f4ce26895
Henryk Paluch <hpaluch@seznam.cz>
2014-07-16 11:56:38 +0200

    Initial build of OTRS 2.4.6

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-# Builds OTRS-3.2 with MySQL in single container - suitable for development
+# Builds OTRS-2.4.6 with MySQL in single container - suitable for development
 # and testing of deployments
 
 # Based on https://github.com/dockerfiles/centos-lamp
@@ -16,10 +16,29 @@
 
 FROM centos:centos6
 
+# install required packages
+RUN yum install -y openssh-server openssh-clients passwd  \
+               mysql-server mysql \
+               httpd mod_ssl && \
+ rpm -ivh \
+ http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm && \
+ yum install -y perl-core perl-Crypt-SSLeay mod_perl procmail mailx \
+         perl-DBI perl-DBD-MySQL perl-TimeDate perl-Net-DNS \
+         perl-IO-Socket-SSL \
+         perl-Convert-ASN1 perl-XML-Parser \
+         perl-GraphViz \
+         perl-GDGraph perl-GDTextUtil  \
+         perl-JSON-XS perl-LDAP perl-PDF-API2 perl-SOAP-Lite \
+         perl-Text-CSV_XS perl-Crypt-PasswdMD5 perl-Digest-SHA \
+         perl-Devel-Symdump \
+         perl-Mail-IMAPClient perl-YAML-LibYAML \
+         perl-MailTools cronie && \
+ rpm -ivh http://ftp.otrs.org/pub/otrs/RPMS/fedora/4/otrs-2.4.6-01.noarch.rpm && \
+ yum install -y python-pip && pip install pip --upgrade && \
+ pip install supervisor
 
 # bootstrap otrs
 ADD setup-otrs /opt/setup-otrs
-RUN /opt/setup-otrs/prepare_os.sh
 RUN /opt/setup-otrs/bootstrap_otrs.sh
 ADD supervisord.conf /etc/
 
--- setup-otrs/bootstrap_otrs.sh
+++ setup-otrs/bootstrap_otrs.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
 set -e
+
+ssh-keygen -q -N "" -t dsa -f /etc/ssh/ssh_host_dsa_key
+ssh-keygen -q -N "" -t rsa -f /etc/ssh/ssh_host_rsa_key
+sed -ri 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
+echo 'root:changeme' | chpasswd
+
+
+sed -i.bak '/\[mysqld\]/amax_allowed_packet=32m' /etc/my.cnf && \
+    sed -i.bak '/\[mysqld\]/aquery_cache_size=10m' /etc/my.cnf
+
 /usr/bin/mysql_install_db --datadir=/var/lib/mysql --user=mysql
 # Workaround for some versions of /etc/init.d/mysqld
 touch /etc/sysconfig/network
-rpm -ivh http://ftp.otrs.org/pub/otrs/RPMS/rhel/6/otrs-3.2.5-01.noarch.rpm 
 /etc/init.d/mysqld start
 mysql -u root mysql < /opt/setup-otrs/create_otrs_db.sql
 mysql -u root otrs < /opt/setup-otrs/otrs_tables.sql
+# avoid SysLog and /dev/log errors
+/opt/setup-otrs/set-config-pm-value.pl LogModule=Kernel::System::Log::File \
+      LogModule::LogFile=/opt/otrs/var/tmp/otrs.log
 /opt/setup-otrs/set-config-pm-value.pl Database=otrs \
       DatabaseUser=otrs DatabasePw=otrs DatabaseHost=127.0.0.1 SecureMode=1
-/opt/otrs/bin/otrs.SetPassword.pl root@localhost root
-/opt/otrs/bin/otrs.PackageManager.pl -a install \
-       -p http://ftp.otrs.org/pub/otrs/packages/:FAQ-2.2.3.opm
-/opt/otrs/bin/otrs.PackageManager.pl -a install \
-       -p http://ftp.otrs.org/pub/otrs/packages/:Support-1.4.5.opm
-/opt/otrs/bin/otrs.PackageManager.pl -a install \
-       -p http://ftp.otrs.org/pub/otrs/itsm/bundle32/:ITSM-3.2.3.opm
+/opt/otrs/bin/otrs.setPassword root@localhost root
+/opt/otrs/bin/opm.pl -a install \
+       -p http://ftp.otrs.org/pub/otrs/packages/:FAQ-1.6.5.opm
+/opt/otrs/bin/opm.pl -a install \
+       -p http://ftp.otrs.org/pub/otrs/packages/:Support-1.0.95.opm
+for i in GeneralCatalog-1.3.2.opm ITSMCore-1.3.2.opm ITSMConfigurationManagement-1.3.2.opm ITSMIncidentProblemManagement-1.3.1.opm ITSMServiceLevelManagement-1.3.2.opm ImportExport-1.3.2.opm
+do
+/opt/otrs/bin/opm.pl -a install \
+       -p http://ftp.otrs.org/pub/otrs/itsm/packages13/:$i
+done
 /etc/init.d/mysqld stop
 chown -R otrs:apache /opt/otrs
 chmod -R g+rwX /opt/otrs
